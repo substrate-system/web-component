71c0d62947aaa00ada0ad8abb2e5f17eb9810937
Nick Thomas <nichoth@nichoth.com>
2024-07-13 07:03:06 -0700

    WIP - broken types

--- example/index.ts
+++ example/index.ts
@@ -8,6 +8,17 @@ declare global {
     interface HTMLElementTagNameMap {
         'my-element': MyElement;
     }
+
+    interface CustomEventMap {
+        'my-element:click': CustomEvent<string>
+    }
+
+    // adds definition to Document, but you can do the same with HTMLElement
+    interface HTMLElement {
+        addEventListener<K extends keyof CustomEventMap>(type: K,
+           listener: (this: Document, ev: CustomEventMap[K]) => void): void;
+        dispatchEvent<K extends keyof CustomEventMap>(ev: CustomEventMap[K]):boolean;
+    }
 }
 
 class MyElement extends WebComponent.create('my-element') {
